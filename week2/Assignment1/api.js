7e738101653cfaf28fe0003dfdd1c76f890375b6
Rolando15 <rolando.hernandez@bryanuniversity.edu>
2022-07-03 18:58:32 -0400

    api images

--- week2/Assignment1/api.js
+++ week2/Assignment1/api.js
@@ -21,5 +21,27 @@ fetch("https://picsum.photos/id/237/200/300").then (response =>{
 
 
 
+    fetch("https://picsum.photos/seed/picsum/200/300").then (response =>{
+        console.log(response);
+        return response.blob();
+    })
+    
+    .then(blob => {
+        
+        document.getElementById('api2').src = URL.createObjectURL(blob)
+            
+        });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
